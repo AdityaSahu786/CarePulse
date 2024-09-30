f76cfca10139905437069d397b23611f28fe35ea
AdityaSahu786 <sahuadi786183@gmail.com>
2024-09-30 22:05:05 +0530

    Update

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,8 @@ import { Input } from "@/components/ui/input"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/router"
 
 export enum FormFieldType  {
     INPUT = 'input',
@@ -27,21 +29,35 @@ const formSchema = z.object({
 })
  
 const PatientForm = () => {
-
+    
+    const router = useRouter();
     const [isLoading, setisLoading] = useState(false);
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
+
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    setisLoading(true);
+
+    try {
+       const userData = { name, email, phone };
+
+       const user = await createUser(userData);
+
+       if(user) router.push{`/patients/${user.$id}/register`} 
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
