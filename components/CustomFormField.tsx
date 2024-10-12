7576d5a80887407fbb47729b2a2f41f3fcf44ddd
AdityaSahu786 <sahuadi786183@gmail.com>
2024-10-12 20:43:25 +0530

    Added Checkbox from shadcn

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -18,6 +18,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 import { Textarea } from "./ui/textarea";
+import { Checkbox } from "./ui/checkbox";
 
 interface CustomProps {
   control: Control<any>,
@@ -129,6 +130,16 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
           </Select>
         </FormControl>
       )
+    case FormFieldType.CHECKBOX:
+         <FormControl>
+            <div className="flex items-center gap-4">
+              <Checkbox 
+                id={props.name}
+                checked={field.value}
+                onCheckedChange={field.onChange}
+              />
+            </div>
+         </FormControl>
     default:
       return null;
   }
